4e53e54c42e8924663302a1924f69b4e10ac7a7d
Virtue Zhao <virtuedizhao@gmail.com>
2023-10-25 23:29:44 +1300

    update

--- train.py
+++ train.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--gpu", type=str, default="0")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = get_args()
+    print(args)
