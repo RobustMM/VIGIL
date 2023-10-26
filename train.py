fa487226efce98d21f9f4e084318dc1002efb131
Virtue Zhao <virtuedizhao@gmail.com>
2023-10-26 23:03:35 +1300

    get_args()

--- train.py
+++ train.py
@@ -3,7 +3,34 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--gpu", type=str, default="0")
+    parser.add_argument(
+        "--gpu",
+        type=str,
+        default="0"
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=42
+    )
+    parser.add_argument(
+        "--dataset",
+        type=str
+    )
+    parser.add_argument(
+        "--source_domain",
+        type=str,
+        nargs="+",
+    )
+    parser.add_argument(
+        "--target_domain",
+        type=str
+    )
+    parser.add_argument(
+        "--root",
+        type=str,
+        default="./data/"
+    )
 
     return parser.parse_args()
 
