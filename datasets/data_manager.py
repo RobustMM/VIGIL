4ba463b98c15974082ac8de99427adbdd9a7d498
Virtue Zhao <virtuedizhao@gmail.com>
2023-11-06 15:58:51 +1300

    build_transform()

--- datasets/data_manager.py
+++ datasets/data_manager.py
@@ -5,4 +5,6 @@ class DataManager:
     def __init__(self, cfg):
         dataset = build_dataset(cfg)
 
-    # TODO: Build Transform
+        # TODO: Build Transform
+        tfm_train = build_transform(cfg, is_train=True)
+        tfm_test = build_transform(cfg, is_train=False)
