d36253f68de55b0bb1e7a8a17f9f5b9244d810ba
Virtue Zhao <virtuedizhao@gmail.com>
2023-11-07 16:27:09 +1300

    build_model()

--- trainer/models/clipzeroshot.py
+++ trainer/models/clipzeroshot.py
@@ -1,8 +1,22 @@
-from trainer import Trainer, MODEL_REGISTERY
+# import clip
+from clip import clip
+
+from trainer import MODEL_REGISTERY, Trainer
+
+PROMPT_TEMPLATES = {"ImageNet": "a photo of a {}."}
 
 
 @MODEL_REGISTERY.register()
 class CLIPZeroshot(Trainer):
     def build_model(self):
-        print("Hi")
+        class_names = self.data_manager.dataset.class_names
+
+        clip_model, preprocess = clip.load(
+            self.cfg.MODEL.BACKBONE,
+            device=self.device,
+            download_root="/data/dzha866/Project/VIGIL/data/",
+        )
+        prompt_template = PROMPT_TEMPLATES[self.cfg.DATASET.NAME]
+        print(prompt_template)
+
         exit()
