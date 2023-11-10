c7f3fca56b37909357488f4a06de68ee1a9ad803
Virtue Zhao <virtuedizhao@gmail.com>
2023-11-10 16:18:24 +1300

    imagenet-a

--- datasets/dg/__init__.py
+++ datasets/dg/__init__.py
@@ -1,4 +1,5 @@
 from .imagenet import ImageNet  # noqa
+from .imagenet_a import ImageNetA  # noqa
+from .imagenet_r import ImageNetR  # noqa
 from .imagenet_sketch import ImageNetSketch  # noqa
 from .imagenetv2 import ImageNetV2  # noqa
-from .imagenet_r import ImageNetR   # noqa
