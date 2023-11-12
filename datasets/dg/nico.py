beccf53ffb7600798b6731ac0105fc763d85accc
Virtue Zhao <virtuedizhao@gmail.com>
2023-11-12 16:44:44 +1300

    update

--- datasets/dg/nico.py
+++ datasets/dg/nico.py
@@ -24,5 +24,41 @@ class NICO(DatasetBase):
         self._dataset_dir = os.path.join(root, self.dataset_dir)
 
         self.check_input_domains(cfg.DATASET.SOURCE_DOMAINS, cfg.DATASET.TARGET_DOMAINS)
-        print("Hi")
+
+        train_data = self._read_data(cfg.DATASET.SOURCE_DOMAINS, "train")
+        exit()
+        val_data = self._read_data(cfg.DATASET.SOURCE_DOMAINS, "test")
+        test_data = self._read_data(cfg.DATASET.TARGET_DOMAINS, "all")
+
+        super().__init__(
+            dataset_dir=self._dataset_dir,
+            domains=self._domains,
+            data_url=self._data_url,
+            train_data=train_data,
+            val_data=val_data,
+            test_data=test_data,
+        )
+
+    def _read_data(self, input_domains, split):
+        def _load_data_from_directory(directory):
+            images_ = []
+
+            with open(directory, "r") as file:
+                lines = file.readlines()
+                for line in lines:
+                    print(line)
+                exit()
+
+        img_datums = []
+
+        for domain_label, domain_name in enumerate(input_domains):
+            if split == "all":
+                pass
+            else:
+                split_dir = os.path.join(
+                    self._dataset_dir, domain_name + "_" + split + ".txt"
+                )
+                img_path_class_label_list = _load_data_from_directory(split_dir)
+                print(len(img_path_class_label_list))
+
         exit()
