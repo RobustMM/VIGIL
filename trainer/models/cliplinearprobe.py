4100e1591e66f7c9892df18e4edf97e2d40815fa
Virtue Zhao <virtuedizhao@gmail.com>
2023-11-17 15:51:17 +1300

    linear_probe

--- trainer/models/cliplinearprobe.py
+++ trainer/models/cliplinearprobe.py
@@ -59,9 +59,48 @@ class CLIPLinearProbe(Trainer):
             acc_list.append(acc_val)
 
         c_peak = search_list[np.argmax(acc_list)]
-        c_left, c_right = 1e1 * c_peak, 1e1 * c_peak
-
-        print(acc_list, flush=True)
         print("C Peak: {}".format(c_peak))
 
-        exit()
+        c_left, c_right = 1e-1 * c_peak, 1e1 * c_peak
+        # Binary search for the best c
+        for _ in range(self.num_step):
+            c_left, c_right = self.binary_search(c_left, c_right)
+
+        self.test()
+
+    def binary_search(self, c_left, c_right):
+        print("Binary Search")
+        print("C Left: {}".format(c_left))
+        print("C Right: {}".format(c_right))
+
+        clf_left = LogisticRegression(
+            solver="lbfgs", max_iter=1000, penalty="l2", C=c_left
+        ).fit(self.embedding_train, self.class_label_train)
+        pred_left = clf_left.predict(self.embedding_val)
+        acc_left = sum(pred_left == self.class_label_val) / len(self.class_label_val)
+        print("Val Accuracy (Left): {:.2f}".format(100 * acc_left))
+
+        clf_right = LogisticRegression(
+            solver="lbfgs", max_iter=1000, penalty="l2", C=c_right
+        ).fit(self.embedding_train, self.class_label_train)
+        pred_right = clf_right.predict(self.embedding_val)
+        acc_right = sum(pred_right == self.class_label_val) / len(self.class_label_val)
+        print("Val Accuracy (Right): {:.2f}".format(100 * acc_right))
+
+        if acc_left < acc_right:
+            self.model = clf_right
+            c_left = 0.5 * (np.log10(c_right) + np.log10(c_left))
+            c_right = np.log10(c_right)
+        else:
+            self.model = clf_left
+            c_right = 0.5 * (np.log10(c_right) + np.log10(c_left))
+            c_left = np.log10(c_left)
+
+        self.test()
+
+        return np.power(10, c_left), np.power(10, c_right)
+
+    def test(self):
+        pred_test = self.model.predict(self.embedding_test)
+        acc_test = sum(pred_test == self.class_label_test) / len(self.class_label_test)
+        print("Test Accuracy: {:.2f}".format(acc_test))
