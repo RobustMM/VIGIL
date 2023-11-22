fb1fa0973a885f011526f41a201d4e04de74619e
Virtue Zhao <virtuedizhao@gmail.com>
2023-11-22 14:46:16 +1300

    format

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
-from .config_default import *   # noqa
-from .logger import *           # noqa
-from .registry import *         # noqa
-from .tools import *            # noqa
-from .prompts import *            # noqa
+from .config_default import *  # noqa
+from .logger import *  # noqa
+from .registry import *  # noqa
+from .tools import *  # noqa
+from .prompts import *  # noqa
